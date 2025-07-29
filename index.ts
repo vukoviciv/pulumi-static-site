83cbe10a589c31578cabba4f75012656f596b61a
Ivana Simic <isimic@extensionengine.com>
2025-07-29 12:42:02 +0200

    Extract bucket creation

--- index.ts
+++ index.ts
@@ -1,61 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as fs from "fs";
-import * as mime from "mime";
+import { S3Bucket } from "./infrastructure/bucket";
+const name = "pulumi-static-v3";
 
-const name = "pulumi-static-v2";
-
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket(`${name}-bucket`, {
-  website: {
-    indexDocument: "index.html",
-  },
-});
-const siteConfig = new aws.s3.BucketWebsiteConfigurationV2(`${name}-config`, {
-  bucket: bucket.id,
-  indexDocument: {
-    suffix: "index.html",
-  },
-});
-const bucketPublicAccessBlock = new aws.s3.BucketPublicAccessBlock(
-  `${name}-bucket-access-block`,
-  {
-    bucket: bucket.id,
-    blockPublicAcls: false,
-    blockPublicPolicy: false,
-    ignorePublicAcls: false,
-    restrictPublicBuckets: false,
-  }
-);
-
-const siteDir = "www";
-// For each file in the directory, create an S3 object stored in `siteBucket`
-for (const item of fs.readdirSync(siteDir)) {
-  const filePath = require("path").join(siteDir, item);
-  const siteObject = new aws.s3.BucketObject(item, {
-    bucket,
-    source: new pulumi.asset.FileAsset(filePath),
-    contentType: mime.getType(filePath) || undefined,
-  });
-}
-const bucketPolicy = new aws.s3.BucketPolicy(
-  `${name}-bucket-policy`,
-  {
-    bucket: bucket.id,
-    policy: pulumi.jsonStringify({
-      Version: "2012-10-17",
-      Statement: [
-        {
-          Effect: "Allow",
-          Principal: "*",
-          Action: ["s3:GetObject"],
-          Resource: [pulumi.interpolate`${bucket.arn}/*`],
-        },
-      ],
-    }),
-  },
-  { dependsOn: bucketPublicAccessBlock }
-);
+const s3bucket = new S3Bucket(name);
 
 ///////// ACM - CERTIFICATE
 const domainName = "ivanasimic.online";
@@ -94,8 +41,8 @@ const cloudfrontDistribution = new aws.cloudfront.Distribution(
   {
     origins: [
       {
-        originId: bucket.arn,
-        domainName: siteConfig.websiteEndpoint,
+        originId: s3bucket.bucket.arn,
+        domainName: s3bucket.siteConfig.websiteEndpoint,
         connectionAttempts: 3,
         connectionTimeout: 10,
         customOriginConfig: {
@@ -111,7 +58,7 @@ const cloudfrontDistribution = new aws.cloudfront.Distribution(
     defaultRootObject: "index.html",
     aliases: [domainName],
     defaultCacheBehavior: {
-      targetOriginId: bucket.arn,
+      targetOriginId: s3bucket.bucket.arn,
       viewerProtocolPolicy: "allow-all",
       allowedMethods: ["GET", "HEAD", "OPTIONS"],
       cachedMethods: ["GET", "HEAD", "OPTIONS"],
@@ -152,5 +99,5 @@ const record = new aws.route53.Record(`${name}-record`, {
 });
 
 // Stack export
-export const bucketName = bucket.id;
-export const websiteUrl = siteConfig.websiteEndpoint;
+export const bucketName = s3bucket.bucket.id;
+export const websiteUrl = s3bucket.siteConfig.websiteEndpoint;
