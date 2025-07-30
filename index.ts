1933d7d2ce62b35eb918550a298029480c5ccda2
Ivana Simic <isimic@extensionengine.com>
2025-07-30 12:42:36 +0200

    revert moving cloudfront and acm

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import * as aws from "@pulumi/aws";
 import { S3Bucket } from "./infrastructure/bucket";
 import { createRecord } from "./infrastructure/route53";
-import { createCloudfrontDistribution } from "./infrastructure/cloudfront";
 
 const name = "pulumi-static-v3";
 
@@ -39,11 +38,52 @@ export const certificateValidation = new aws.acm.CertificateValidation(
   }
 );
 
-const cloudfrontDistribution = createCloudfrontDistribution(
-  name,
-  s3bucket,
-  certificateValidation,
-  domainName
+const cloudfrontDistribution = new aws.cloudfront.Distribution(
+  `${name}-cloudfront`,
+  {
+    origins: [
+      {
+        originId: s3bucket.bucket.arn,
+        domainName: s3bucket.siteConfig.websiteEndpoint,
+        connectionAttempts: 3,
+        connectionTimeout: 10,
+        customOriginConfig: {
+          httpPort: 80,
+          httpsPort: 443,
+          originProtocolPolicy: "http-only",
+          originSslProtocols: ["TLSv1.2"],
+        },
+      },
+    ],
+    enabled: true,
+    isIpv6Enabled: true,
+    defaultRootObject: "index.html",
+    aliases: [domainName],
+    defaultCacheBehavior: {
+      targetOriginId: s3bucket.bucket.arn,
+      viewerProtocolPolicy: "allow-all",
+      allowedMethods: ["GET", "HEAD", "OPTIONS"],
+      cachedMethods: ["GET", "HEAD", "OPTIONS"],
+      minTtl: 0,
+      defaultTtl: 3600,
+      maxTtl: 86400,
+      forwardedValues: {
+        queryString: false,
+        cookies: { forward: "none" },
+      },
+    },
+    tags: {
+      Environment: "development",
+    },
+    viewerCertificate: {
+      acmCertificateArn: certificateValidation.certificateArn,
+      sslSupportMethod: "sni-only",
+      minimumProtocolVersion: "TLSv1.2_2021",
+    },
+    restrictions: {
+      geoRestriction: { restrictionType: "none" },
+    },
+  }
 );
 
 const record = createRecord(
