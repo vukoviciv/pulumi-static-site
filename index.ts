9cd2454def45bd8cb8b5fc71e215277efa600a88
Ivana Simic <isimic@extensionengine.com>
2025-07-31 12:06:50 +0200

    Extract cloudfront

--- index.ts
+++ index.ts
@@ -2,64 +2,20 @@ import * as aws from "@pulumi/aws";
 import { S3Bucket } from "./infrastructure/bucket";
 import { createRecord } from "./infrastructure/route53";
 import { AcmCertificate } from "./infrastructure/acm-certificate";
+import { createCloudfrontDistribution } from "./infrastructure/cloudfront";
 
-const name = "pulumi-static-v4";
+const name = "pulumi-static-v5";
 
 const s3bucket = new S3Bucket(name);
-
 const domainName = "ivanasimic.online";
 const hostedZone = aws.route53.getZoneOutput({ name: domainName });
 
 const certificate = new AcmCertificate(domainName, hostedZone.id);
-
-// Cloudfront
-const cloudfrontDistribution = new aws.cloudfront.Distribution(
-  `${name}-cloudfront`,
-  {
-    origins: [
-      {
-        originId: s3bucket.bucket.arn,
-        domainName: s3bucket.siteConfig.websiteEndpoint,
-        connectionAttempts: 3,
-        connectionTimeout: 10,
-        customOriginConfig: {
-          httpPort: 80,
-          httpsPort: 443,
-          originProtocolPolicy: "http-only",
-          originSslProtocols: ["TLSv1.2"],
-        },
-      },
-    ],
-    enabled: true,
-    isIpv6Enabled: true,
-    defaultRootObject: "index.html",
-    aliases: [domainName],
-    defaultCacheBehavior: {
-      targetOriginId: s3bucket.bucket.arn,
-      viewerProtocolPolicy: "allow-all",
-      allowedMethods: ["GET", "HEAD", "OPTIONS"],
-      cachedMethods: ["GET", "HEAD", "OPTIONS"],
-      minTtl: 0,
-      defaultTtl: 3600,
-      maxTtl: 86400,
-      forwardedValues: {
-        queryString: false,
-        cookies: { forward: "none" },
-      },
-    },
-    tags: {
-      Environment: "development",
-    },
-    viewerCertificate: {
-      acmCertificateArn: certificate.validation.certificateArn,
-      sslSupportMethod: "sni-only",
-      minimumProtocolVersion: "TLSv1.2_2021",
-    },
-    restrictions: {
-      geoRestriction: { restrictionType: "none" },
-    },
-  },
-  { deleteBeforeReplace: true, dependsOn: [certificate.validation] }
+const cloudfrontDistribution = createCloudfrontDistribution(
+  name,
+  s3bucket,
+  certificate.validation,
+  domainName
 );
 
 const record = createRecord(
@@ -69,6 +25,5 @@ const record = createRecord(
   cloudfrontDistribution
 );
 
-// Stack export
 export const bucketName = s3bucket.bucket.id;
 export const websiteUrl = s3bucket.siteConfig.websiteEndpoint;
--- infrastructure/cloudfront.ts
+++ infrastructure/cloudfront.ts
@@ -7,48 +7,52 @@ export const createCloudfrontDistribution = (
   certificateValidation: acm.CertificateValidation,
   domainName: string
 ) => {
-  return new cloudfront.Distribution(`${name}-cloudfront`, {
-    origins: [
-      {
-        originId: s3bucket.bucket.arn,
-        domainName: s3bucket.siteConfig.websiteEndpoint,
-        connectionAttempts: 3,
-        connectionTimeout: 10,
-        customOriginConfig: {
-          httpPort: 80,
-          httpsPort: 443,
-          originProtocolPolicy: "http-only",
-          originSslProtocols: ["TLSv1.2"],
+  return new cloudfront.Distribution(
+    `${name}-cloudfront`,
+    {
+      origins: [
+        {
+          originId: s3bucket.bucket.arn,
+          domainName: s3bucket.siteConfig.websiteEndpoint,
+          connectionAttempts: 3,
+          connectionTimeout: 10,
+          customOriginConfig: {
+            httpPort: 80,
+            httpsPort: 443,
+            originProtocolPolicy: "http-only",
+            originSslProtocols: ["TLSv1.2"],
+          },
+        },
+      ],
+      enabled: true,
+      isIpv6Enabled: true,
+      defaultRootObject: "index.html",
+      aliases: [domainName],
+      defaultCacheBehavior: {
+        targetOriginId: s3bucket.bucket.arn,
+        viewerProtocolPolicy: "allow-all",
+        allowedMethods: ["GET", "HEAD", "OPTIONS"],
+        cachedMethods: ["GET", "HEAD", "OPTIONS"],
+        minTtl: 0,
+        defaultTtl: 3600,
+        maxTtl: 86400,
+        forwardedValues: {
+          queryString: false,
+          cookies: { forward: "none" },
         },
       },
-    ],
-    enabled: true,
-    isIpv6Enabled: true,
-    defaultRootObject: "index.html",
-    aliases: [domainName],
-    defaultCacheBehavior: {
-      targetOriginId: s3bucket.bucket.arn,
-      viewerProtocolPolicy: "allow-all",
-      allowedMethods: ["GET", "HEAD", "OPTIONS"],
-      cachedMethods: ["GET", "HEAD", "OPTIONS"],
-      minTtl: 0,
-      defaultTtl: 3600,
-      maxTtl: 86400,
-      forwardedValues: {
-        queryString: false,
-        cookies: { forward: "none" },
+      tags: {
+        Environment: "development",
+      },
+      viewerCertificate: {
+        acmCertificateArn: certificateValidation.certificateArn,
+        sslSupportMethod: "sni-only",
+        minimumProtocolVersion: "TLSv1.2_2021",
+      },
+      restrictions: {
+        geoRestriction: { restrictionType: "none" },
       },
     },
-    tags: {
-      Environment: "development",
-    },
-    viewerCertificate: {
-      acmCertificateArn: certificateValidation.certificateArn,
-      sslSupportMethod: "sni-only",
-      minimumProtocolVersion: "TLSv1.2_2021",
-    },
-    restrictions: {
-      geoRestriction: { restrictionType: "none" },
-    },
-  });
+    { deleteBeforeReplace: true, dependsOn: [certificateValidation] }
+  );
 };
