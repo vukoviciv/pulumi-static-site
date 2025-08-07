172e5ab76ec31cff78bd03e0b80a429b2e5863d4
Ivana Simic <isimic@extensionengine.com>
2025-08-07 12:35:33 +0200

    cleanup

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -11,10 +11,7 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <p>Testis testis</p>
   </div>
 
   <p>
