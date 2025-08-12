986ad28470fcba9795aba7436374edd2111c140e
Ivana Simic <isimic@extensionengine.com>
2025-08-12 14:06:40 +0200

    Simplify homepage

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,4 @@
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld :msg="msg" />
   <button @click="handleHello">Click for hello</button>
   <button @click="handleGoodbye" class="margin-left">Click for goodbye</button>
@@ -32,18 +24,5 @@ const handleGoodbye = async () => {
 <style scoped>
 .margin-left {
   margin-left: 1rem;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
