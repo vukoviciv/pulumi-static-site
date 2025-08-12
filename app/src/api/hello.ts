e3bc28c41e0123e688c767f9d4e1d702e6ac01ed
Ivana Simic <isimic@extensionengine.com>
2025-08-12 14:13:04 +0200

    Move request to separate file

--- app/src/api/hello.ts
+++ app/src/api/hello.ts
@@ -1,13 +1,6 @@
-import axios from "axios";
+import request from "./request";
 
-const request = axios.create({
-  baseURL: "http://localhost:3000",
-  headers: { "Content-Type": "application/json" },
-});
-
-const urls = {
-  root: "/hello",
-};
+const urls = { root: "/hello" };
 
 export async function get() {
   return request.get(urls.root).then((res) => res.data);
